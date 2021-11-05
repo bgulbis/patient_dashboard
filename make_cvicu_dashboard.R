c3581c22b44bfe9ced18e25207a6181e769307fa
Brian Gulbis <briangulbis@alisons-air.mh.org>
2021-11-05 08:24:39 -0500

    note for network drives

--- make_cvicu_dashboard.R
+++ make_cvicu_dashboard.R
@@ -3,3 +3,6 @@ rmarkdown::render(
     output_file = "dashboard_cvicu.html",
     output_dir = "/Users/briangulbis/Share/U"
 )
+
+# mount_smbfs //brgulbis@tmcisilon/user$/user1/brgulbis /Users/briangulbis/Share/U
+# mount_smbfs //brgulbis@mh.org/public /Users/briangulbis/Share/W
