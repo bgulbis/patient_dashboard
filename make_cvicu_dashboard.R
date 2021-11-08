333028558d4c9f2a401b5bff90ca4b6895af78fe
Brian Gulbis <briangulbis@Alisons-MacBook-Air.local>
2021-11-08 08:17:26 -0600

    update path to data

--- make_cvicu_dashboard.R
+++ make_cvicu_dashboard.R
@@ -1,8 +1,9 @@
 rmarkdown::render(
     input = "report/daily_dashboard_cvicu.Rmd",
     output_file = "dashboard_cvicu.html",
-    output_dir = "/Users/briangulbis/Share/U"
+    # output_dir = "/Users/briangulbis/Share/U"
+    output_dir = "/Volumes/brgulbis/"
 )
 
 # mount_smbfs //brgulbis@tmcisilon.mh.org/user$/user1/brgulbis /Users/briangulbis/Share/U
-# mount_smbfs //brgulbis@mhdc01.mh.org/public /Users/briangulbis/Share/W
+# mount_smbfs //brgulbis@mh.org/public /Users/briangulbis/Share/W
