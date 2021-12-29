f58556fb86462cfd4b0a5f9f67c08415112e1806
Brian Gulbis <brian.gulbis@gmail.com>
2021-12-29 07:41:08 -0600

    fix output dir

--- make_ccu_dashboard.R
+++ make_ccu_dashboard.R
@@ -1,6 +1,6 @@
 rmarkdown::render(
     input = "report/daily_dashboard_ccu.Rmd",
     output_file = "dashboard_ccu.html",
-    output_dir = "U:"
+    output_dir = "U:/"
     # output_dir = "/Volumes/brgulbis/"
 )
--- make_cvicu_dashboard.R
+++ make_cvicu_dashboard.R
@@ -1,6 +1,6 @@
 rmarkdown::render(
     input = "report/daily_dashboard_cvicu.Rmd",
     output_file = "dashboard_cvicu.html",
-    output_dir = "U:"
+    output_dir = "U:/"
     # output_dir = "/Volumes/brgulbis/"
 )
