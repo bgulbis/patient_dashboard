4c060505a4f81c7fbd6fd47a31c3410811c9b91d
Brian Gulbis <brian.gulbis@gmail.com>
2022-01-03 20:56:57 -0600

    set data path based on os

--- make_cvicu_dashboard.R
+++
@@ -1,6 +0,0 @@
-rmarkdown::render(
-    input = "report/daily_dashboard_cvicu.Rmd",
-    output_file = "dashboard_cvicu.html",
-    output_dir = "U:/"
-    # output_dir = "/Volumes/brgulbis/"
-)
--- make_dashboards.R
+++ make_dashboards.R
@@ -1,13 +1,22 @@
+# set output directory
+if (Sys.info()['sysname'] == "Windows") {
+    f <- "U:/"
+} else if (Sys.info()['sysname'] == "Darwin") { # macOS
+    f <- "/Volumes/brgulbis/"
+}
+
+if (!dir.exists(f)) {
+    stop("Network drive not available.")
+}
+
 rmarkdown::render(
     input = "report/daily_dashboard_cvicu.Rmd",
     output_file = "dashboard_cvicu.html",
-    output_dir = "U:/"
-    # output_dir = "/Volumes/brgulbis/"
+    output_dir = f
 )
 
 rmarkdown::render(
     input = "report/daily_dashboard_ccu.Rmd",
     output_file = "dashboard_ccu.html",
-    output_dir = "U:/"
-    # output_dir = "/Volumes/brgulbis/"
+    output_dir = f
 )
