31f7581eda231e7b60ff5247c0665cb763a5ebb2
bgulbis <brian.gulbis@gmail.com>
2024-05-30 14:42:45 -0500

    add stop if data out of date

--- make_dashboards.R
+++ make_dashboards.R
@@ -1,3 +1,5 @@
+library(lubridate)
+
 # set output directory
 if (Sys.info()['sysname'] == "Windows") {
     f <- "U:/"
@@ -7,6 +9,18 @@ if (Sys.info()['sysname'] == "Windows") {
 
 if (!dir.exists(f)) {
     stop("Network drive not available.")
+}
+
+if (Sys.info()['sysname'] == "Windows") {
+    file_nm <- "U:/Data/patient_dashboard/dashboard_data_daily.xlsx"
+} else if (Sys.info()['sysname'] == "Darwin") { # macOS
+    f <- "/Volumes/brgulbis/Data/patient_dashboard/dashboard_data_daily.xlsx"
+}
+
+if (as_date(file.info(file_nm)$mtime) < today()) {
+    warning("Data from previous day")
+} else if (as_datetime(file.info(paste0(f, "dashboard_cvicu.html"))$mtime) > as_datetime(file.info(file_nm)$mtime)) {
+    stop(paste("Dashboard data out-of-date. Last updated:", format(file.info(file_nm)$mtime, "%B %d, %Y at %I:%M %p")))
 }
 
 rmarkdown::render(
@@ -26,11 +40,5 @@ rmarkdown::render(
     output_file = "dashboard_ccu.html",
     output_dir = f
 )
-
-if (as_date(file.info(file_nm)$mtime) < today()) {
-    warning("Data from previous day")
-}
-
-file_nm <- paste0(f, "Data/patient_dashboard/dashboard_data_daily.xlsx")
 
 message(paste("Data updated:", format(file.info(file_nm)$mtime, "%B %d, %Y at %I:%M %p")))
